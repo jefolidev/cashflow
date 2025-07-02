a796bca91a1d5874c238156e1efa564a9c5823af
jefolidev <jefoliveira279@gmail.com>
2025-07-02 19:19:16 -0300

    echo: first commit

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,42 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpense Execute(RequestRegisterExpense request)
+    {
+        Validate(request);
+        return new ResponseRegisteredExpense();
+    }
+
+    public void Validate(RequestRegisterExpense request)
+    {
+        var isTitleEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if (isTitleEmpty)
+        {
+            throw new ArgumentException("Title is required.");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+
+        var isExpenseFromToday = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if(isExpenseFromToday > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var isValidPaymentType = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!isValidPaymentType)
+        {
+            throw new ArgumentException("Incorrect Payment Type.");
+        }
+    }
+}
---
+++ src/CashFlow/Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpense request)
+    {
+        var sut = new RegisterExpenseUseCase();
+
+        var response = sut.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+
+}
