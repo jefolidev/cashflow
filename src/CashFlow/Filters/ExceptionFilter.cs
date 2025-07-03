63fe010c82d21c634382fcd8e22d8df591cb6b05
jefolidev <jefoliveira279@gmail.com>
2025-07-03 17:41:52 -0300

    feature: added exception filter and added with it a handle to exceptions

---
+++ src/CashFlow/Filters/ExceptionFilter.cs
@@ -0,0 +1,45 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+
+        }
+        else
+        {
+            ThrowUnknownException(context);
+        }
+
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            ErrorOnValidationException ex = (ErrorOnValidationException)context.Exception;
+
+            ErrorOnValidationException error = new(ex.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(error);
+
+        }
+    }
+
+    private void ThrowUnknownException(ExceptionContext context)
+    {
+        ResponseError error = new("Unknown error.");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(error);
+    }
+}
